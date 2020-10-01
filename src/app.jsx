105cd7e44bff579db161515c4f7a23b806ab17ae
Muhammad Rizki Fikriansyah <fikriansyahmr@gmail.com>
2020-10-01 16:38:08 +0200

    Entry point of the app jsx

---
+++ src/app.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import IndecisionApp from './components/IndecisionApp.jsx';
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
