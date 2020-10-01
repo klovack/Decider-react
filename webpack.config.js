c4d035ce5ca345032a93c37188adc37ffd1bded4
Muhammad Rizki Fikriansyah <fikriansyahmr@gmail.com>
2020-10-01 16:36:47 +0200

    Initial commit with all necessary config files

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.jsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
